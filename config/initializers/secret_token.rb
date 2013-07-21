01820259ddc29599983ee6cb8d6b63e259948fd0
karthy07 <mail2karthyk@gmail.com>
2013-07-21 19:06:40 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AbilityMatrimony::Application.config.secret_token = '4ffbf681475b9ce1541aa56565103fdcd302dc0d92a59d7645d485a687b38753641ff389708ef62a546cf9d4b96549294e32b9474c56a857a66eef559379e14a'
